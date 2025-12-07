7bb614af85534d46bd4f84a30093ff3f04f33b93
GH1hatem <gharsallahhatem58@gmail.com>
2025-12-07 11:19:59 +0100

    fix backslash

--- order_management/urls.py
+++ order_management/urls.py
@@ -8,8 +8,8 @@ from .views import (
 )
 
 urlpatterns = [
-    path("orders/create", OrderCreateView.as_view(), name="order-create"),
-    path("orders/update/<int:pk>", OrderUpdateView.as_view(), name="order-update"),
-    path("oms/invoice", InvoiceCreateView.as_view(), name="invoice-create"),
-    path("oms/fulfillment", FulfillmentRequestCreateView.as_view(), name="fulfillment-create"),
+    path("orders/create/", OrderCreateView.as_view(), name="order-create"),
+    path("orders/update/<int:pk>/", OrderUpdateView.as_view(), name="order-update"),
+    path("oms/invoice/", InvoiceCreateView.as_view(), name="invoice-create"),
+    path("oms/fulfillment/", FulfillmentRequestCreateView.as_view(), name="fulfillment-create"),
 ]
